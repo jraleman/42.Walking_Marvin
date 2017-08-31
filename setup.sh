c67e251f458e0ee0064ff9c53cba8ccf85906d48
jraleman <jraleman@bendiburg.com>
2017-08-31 16:12:07 -0700

    Updating setup.sh, we don't longer need logzero, let's use the default one

--- setup.sh
+++ setup.sh
@@ -13,13 +13,12 @@
 # **************************************************************************** #
 
 # Directories variables.
-BIN_DIR="marvin"
+BIN_DIR="."
 GYM_DIR="gym"
 RES_DIR="resources"
-LOG_DIR="logzero"
 
 # Check if the user uses a Mac.
-if [ $OS_NAME == "Darwin" ] ; then
+if [ $(uname -s) = "Darwin" ] ; then
     echo " > Updating packages / installing python..."
     brew update
     brew install python3
@@ -47,40 +46,18 @@ cd $BIN_DIR/$GYM_DIR/
 # Install the OpenAI Gym package.
 echo " > Installing the OpenAI Gym package..."
 pip install --user -e .
-cd ../../
-
+cd ../
+#
 # Extracts all the files needed for the walking marvin enviroment.
 echo " > Extracting and installing the walking marvin enviroment..."
 cd $RES_DIR/
 unzip envs.zip
 cd ../
-cp -Rf $RES_DIR/envs $BIN_DIR/$GYM_DIR/gym/envs/
+cp -Rf $RES_DIR/envs $BIN_DIR/$GYM_DIR/gym/
 
 # Remove extracted files.
 echo " > Cleaning files..."
 rm -Rf $RES_DIR/envs/
-
-# Check if there's a previous installation already.
-if [ -d $BIN_DIR/$LOG_DIR ] ; then
-    echo " > It seems that logzero is already installed."
-    while true; do
-        read -p "Do you wish to overwrite the installation? [y / n] " yn
-        case $yn in
-            [Yy]* ) echo " > Cleaning previous installation..."; \
-                    rm -Rf $BIN_DIR/$LOG_DIR/; break;;
-            [Nn]* ) echo " > Nothing has been modified. Bye :)"; exit;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-fi
-
-# Install logzero package.
-echo " > Installing the logzero package..."
-git clone https://github.com/metachris/logzero.git $BIN_DIR/$LOG_DIR/
-cd $BIN_DIR/$LOG_DIR/
-pip install --user setuptools
-pip install --user -e .
-cd ../../
 
 # Shows a success message :D
 echo " > Done! Everything is pepa."
