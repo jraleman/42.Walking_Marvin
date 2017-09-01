9702c3dc002c731a3a4148ee892017169b74e4ad
jraleman <jraleman@bendiburg.com>
2017-09-01 15:01:26 -0700

    Updating walking test, need to fix generation problem for mac

--- tests/walking_test.py
+++ tests/walking_test.py
@@ -624,89 +624,7 @@ class Marvin(gym.Env):
         return self.viewer.render(return_rgb_array = mode=='rgb_array')
 
 
-# if __name__=="__main__":
-#     # Heurisic: suboptimal, have no notion of balance.
-#     env = Marvin()
-#     env.reset()
-#     steps = 0
-#     total_reward = 0
-#     a = np.array([0.0, 0.0, 0.0, 0.0])
-#     STAY_ON_ONE_LEG, PUT_OTHER_DOWN, PUSH_OFF = 1,2,3
-#     SPEED = 0.29  # Will fall forward on higher speed
-#     state = STAY_ON_ONE_LEG
-#     moving_leg = 0
-#     supporting_leg = 1 - moving_leg
-#     SUPPORT_KNEE_ANGLE = +0.1
-#     supporting_knee_angle = SUPPORT_KNEE_ANGLE
-#     while True:
-#         s, r, done, info = env.step(a)
-#         total_reward += r
-#         if steps % 20 == 0 or done:
-#             print("\naction " + str(["{:+0.2f}".format(x) for x in a]))
-#             print("step {} total_reward {:+0.2f}".format(steps, total_reward))
-#             print("hull " + str(["{:+0.2f}".format(x) for x in s[0:4] ]))
-#             print("leg0 " + str(["{:+0.2f}".format(x) for x in s[4:9] ]))
-#             print("leg1 " + str(["{:+0.2f}".format(x) for x in s[9:14]]))
-#         steps += 1
-#
-#         contact0 = s[8]
-#         contact1 = s[13]
-#         moving_s_base = 4 + 5*moving_leg
-#         supporting_s_base = 4 + 5*supporting_leg
-#
-#         hip_targ  = [None,None]   # -0.8 .. +1.1
-#         knee_targ = [None,None]   # -0.6 .. +0.9
-#         hip_todo  = [0.0, 0.0]
-#         knee_todo = [0.0, 0.0]
-#
-#         if state==STAY_ON_ONE_LEG:
-#             hip_targ[moving_leg]  = 1.1
-#             knee_targ[moving_leg] = -0.6
-#             supporting_knee_angle += 0.03
-#             if s[2] > SPEED: supporting_knee_angle += 0.03
-#             supporting_knee_angle = min( supporting_knee_angle, SUPPORT_KNEE_ANGLE )
-#             knee_targ[supporting_leg] = supporting_knee_angle
-#             if s[supporting_s_base+0] < 0.10: # supporting leg is behind
-#                 state = PUT_OTHER_DOWN
-#         if state==PUT_OTHER_DOWN:
-#             hip_targ[moving_leg]  = +0.1
-#             knee_targ[moving_leg] = SUPPORT_KNEE_ANGLE
-#             knee_targ[supporting_leg] = supporting_knee_angle
-#             if s[moving_s_base+4]:
-#                 state = PUSH_OFF
-#                 supporting_knee_angle = min( s[moving_s_base+2], SUPPORT_KNEE_ANGLE )
-#         if state==PUSH_OFF:
-#             knee_targ[moving_leg] = supporting_knee_angle
-#             knee_targ[supporting_leg] = +1.0
-#             if s[supporting_s_base+2] > 0.88 or s[2] > 1.2*SPEED:
-#                 state = STAY_ON_ONE_LEG
-#                 moving_leg = 1 - moving_leg
-#                 supporting_leg = 1 - moving_leg
-#
-#         if hip_targ[0]: hip_todo[0] = 0.9*(hip_targ[0] - s[4]) - 0.25*s[5]
-#         if hip_targ[1]: hip_todo[1] = 0.9*(hip_targ[1] - s[9]) - 0.25*s[10]
-#         if knee_targ[0]: knee_todo[0] = 4.0*(knee_targ[0] - s[6])  - 0.25*s[7]
-#         if knee_targ[1]: knee_todo[1] = 4.0*(knee_targ[1] - s[11]) - 0.25*s[12]
-#
-#         hip_todo[0] -= 0.9*(0-s[0]) - 1.5*s[1] # PID to keep head strait
-#         hip_todo[1] -= 0.9*(0-s[0]) - 1.5*s[1]
-#         knee_todo[0] -= 15.0*s[3]  # vertical speed, to damp oscillations
-#         knee_todo[1] -= 15.0*s[3]
-#
-#         a[0] = hip_todo[0]
-#         a[1] = knee_todo[0]
-#         a[2] = hip_todo[1]
-#         a[3] = knee_todo[1]
-#         a = np.clip(0.5*a, -1.0, 1.0)
-#
-#         env.render()
-#         if done: break
-
-
-
-
-
-
+################################################################################
 
 
 
@@ -738,7 +656,10 @@ class Marvin(gym.Env):
 import time, math, random, bisect, copy
 import gym
 import numpy as np
+from gym import wrappers
 
+
+################################################################################
 
 class NeuralNet :
     def __init__(self, nodeCount):
@@ -780,6 +701,9 @@ class NeuralNet :
         for i in range(len(self.nodeCount)-1):
             output = np.reshape( np.dot(output, self.weights[i]) + self.biases[i], (self.nodeCount[i+1]))
         return output
+
+
+################################################################################
 
 
 class Population :
@@ -847,6 +771,10 @@ def sigmoid(x):
     return 1.0/(1.0 + np.exp(-x))
 
 
+################################################################################
+
+
+
 def replayBestBots(bestNeuralNets, steps, sleep):
     choice = input("Do you want to watch the replay ?[Y/N] : ")
     if choice=='Y' or choice=='y':
@@ -866,10 +794,11 @@ def replayBestBots(bestNeuralNets, steps, sleep):
                 print("Generation %3d | Expected Fitness of %4d | Actual Fitness = %4d" % (i+1, bestNeuralNets[i].fitness, totalReward))
 
 
-def recordBestBots(bestNeuralNets):
+def recordBestBots(bestNeuralNets, env):
+    #env = wrappers.Monitor(env, './videos', force='True')
     print("\n Recording Best Bots ")
     print("---------------------")
-    env.monitor.start('Artificial Intelligence/'+GAME, force=True)
+    env = gym.wrappers.Monitor(env, 'Artificial Intelligence/'+GAME, force=True)
     observation = env.reset()
     for i in range(len(bestNeuralNets)):
         totalReward = 0
@@ -885,9 +814,16 @@ def recordBestBots(bestNeuralNets):
     env.monitor.close()
 
 
-def uploadSimulation():
-    API_KEY = open('/home/dollarakshay/Documents/API Keys/Open AI Key.txt', 'r').read().rstrip()
-    gym.upload('Artificial Intelligence/'+GAME, api_key=API_KEY)
+
+#def uploadSimulation():
+#    API_KEY = open('/home/dollarakshay/Documents/API Keys/Open AI Key.txt', 'r').read().rstrip()
+#    gym.upload('Artificial Intelligence/'+GAME, api_key=API_KEY)
+
+
+
+
+################################################################################
+
 
 
 
@@ -918,32 +854,17 @@ def scaleArray(aVal, aMin, aMax):
 
 
 
-# from sets import Set
-#
-#
-# try:
-#     from operator import matmul
-# except ImportError:
-#     def matmul(a, b):
-#         # subtypes have priority:
-#         reversed_first = np.issubtype(type(b), type(a))
-#         if reversed_first:
-#             res = b.__rmatmul__(a)
-#         else:
-#             res = a.__matmul__(b)
-#
-#         if res is not NotImplemented:
-#             return res
-#         # Try the opposite way around, since NotImplemented was returned:
-#         if reversed_first:
-#              ret = a.__matmul__(b)
-#         else:
-#             ret = b.__rmatmul__(a)
-#
-#         if ret is not NotImplemented:
-#             return ret
-#         return False
 
+
+
+################################################################################
+
+# Global variables
+GAME = 'Marvin-v0'
+MAX_STEPS = 1000
+MAX_GENERATIONS = 30
+POPULATION_COUNT = 1000
+MUTATION_RATE = 0.042
 
 
 if __name__=="__main__":
@@ -951,20 +872,30 @@ if __name__=="__main__":
 
 
 
-    GAME = 'BipedalWalker-v2'
-    MAX_STEPS = 1000
-    MAX_GENERATIONS = 10
-    POPULATION_COUNT = 5
-    MUTATION_RATE = 0.01
-    env = Marvin()
+
+
+
+
+
+    env = gym.make(GAME)
+    #env = Marvin()
     #env.reset()
+    #env = TraceRecordingWrapper(env)
+    #env = wrappers.Monitor(env, './videos', force='True')
+
+
+
     observation = env.reset()
     in_dimen = env.observation_space.shape[0]
     out_dimen = env.action_space.shape[0]
     obsMin = env.observation_space.low
     obsMax = env.observation_space.high
+
+
     actionMin = env.action_space.low
     actionMax = env.action_space.high
+
+
     pop = Population(POPULATION_COUNT, MUTATION_RATE, [in_dimen, 13, 8, 13, out_dimen])
     bestNeuralNets = []
 
@@ -983,7 +914,7 @@ if __name__=="__main__":
             observation = env.reset()
             totalReward = 0
             for step in range(MAX_STEPS):
-                env.render()
+                #env.render()
                 action = nn.getOutput(observation)
                 observation, reward, done, info = env.step(action)
                 totalReward += reward
@@ -1004,87 +935,8 @@ if __name__=="__main__":
         #env.render()
         #if done: break
 
-    recordBestBots(bestNeuralNets)
+    recordBestBots(bestNeuralNets, env)
 
-    uploadSimulation()
+    #uploadSimulation()
 
     replayBestBots(bestNeuralNets, max(1, int(math.ceil(MAX_GENERATIONS/10.0))), 0.0625)
-
-
-    #
-    #
-    # env = Marvin()
-    # env.reset()
-    # steps = 0
-    # total_reward = 0
-    # a = np.array([0.0, 0.0, 0.0, 0.0])
-    # STAY_ON_ONE_LEG, PUT_OTHER_DOWN, PUSH_OFF = 1,2,3
-    # SPEED = 0.29  # Will fall forward on higher speed
-    # state = STAY_ON_ONE_LEG
-    # moving_leg = 0
-    # supporting_leg = 1 - moving_leg
-    # SUPPORT_KNEE_ANGLE = +0.1
-    # supporting_knee_angle = SUPPORT_KNEE_ANGLE
-    # while True:
-    #     s, r, done, info = env.step(a)
-    #     total_reward += r
-    #     if steps % 20 == 0 or done:
-    #         print("\naction " + str(["{:+0.2f}".format(x) for x in a]))
-    #         print("step {} total_reward {:+0.2f}".format(steps, total_reward))
-    #         print("hull " + str(["{:+0.2f}".format(x) for x in s[0:4] ]))
-    #         print("leg0 " + str(["{:+0.2f}".format(x) for x in s[4:9] ]))
-    #         print("leg1 " + str(["{:+0.2f}".format(x) for x in s[9:14]]))
-    #     steps += 1
-    #
-    #     contact0 = s[8]
-    #     contact1 = s[13]
-    #     moving_s_base = 4 + 5*moving_leg
-    #     supporting_s_base = 4 + 5*supporting_leg
-    #
-    #     hip_targ  = [None,None]   # -0.8 .. +1.1
-    #     knee_targ = [None,None]   # -0.6 .. +0.9
-    #     hip_todo  = [0.0, 0.0]
-    #     knee_todo = [0.0, 0.0]
-    #
-    #     if state==STAY_ON_ONE_LEG:
-    #         hip_targ[moving_leg]  = 1.1
-    #         knee_targ[moving_leg] = -0.6
-    #         supporting_knee_angle += 0.03
-    #         if s[2] > SPEED: supporting_knee_angle += 0.03
-    #         supporting_knee_angle = min( supporting_knee_angle, SUPPORT_KNEE_ANGLE )
-    #         knee_targ[supporting_leg] = supporting_knee_angle
-    #         if s[supporting_s_base+0] < 0.10: # supporting leg is behind
-    #             state = PUT_OTHER_DOWN
-    #     if state==PUT_OTHER_DOWN:
-    #         hip_targ[moving_leg]  = +0.1
-    #         knee_targ[moving_leg] = SUPPORT_KNEE_ANGLE
-    #         knee_targ[supporting_leg] = supporting_knee_angle
-    #         if s[moving_s_base+4]:
-    #             state = PUSH_OFF
-    #             supporting_knee_angle = min( s[moving_s_base+2], SUPPORT_KNEE_ANGLE )
-    #     if state==PUSH_OFF:
-    #         knee_targ[moving_leg] = supporting_knee_angle
-    #         knee_targ[supporting_leg] = +1.0
-    #         if s[supporting_s_base+2] > 0.88 or s[2] > 1.2*SPEED:
-    #             state = STAY_ON_ONE_LEG
-    #             moving_leg = 1 - moving_leg
-    #             supporting_leg = 1 - moving_leg
-    #
-    #     if hip_targ[0]: hip_todo[0] = 0.9*(hip_targ[0] - s[4]) - 0.25*s[5]
-    #     if hip_targ[1]: hip_todo[1] = 0.9*(hip_targ[1] - s[9]) - 0.25*s[10]
-    #     if knee_targ[0]: knee_todo[0] = 4.0*(knee_targ[0] - s[6])  - 0.25*s[7]
-    #     if knee_targ[1]: knee_todo[1] = 4.0*(knee_targ[1] - s[11]) - 0.25*s[12]
-    #
-    #     hip_todo[0] -= 0.9*(0-s[0]) - 1.5*s[1] # PID to keep head strait
-    #     hip_todo[1] -= 0.9*(0-s[0]) - 1.5*s[1]
-    #     knee_todo[0] -= 15.0*s[3]  # vertical speed, to damp oscillations
-    #     knee_todo[1] -= 15.0*s[3]
-    #
-    #     a[0] = hip_todo[0]
-    #     a[1] = knee_todo[0]
-    #     a[2] = hip_todo[1]
-    #     a[3] = knee_todo[1]
-    #     a = np.clip(0.5*a, -1.0, 1.0)
-    #
-    #     env.render()
-    #     if done: break
