5567175e7cfaf98241fb3d22bcb100d5f73d96b0
jraleman <jraleman@bendiburg.com>
2017-09-05 02:18:40 -0700

    Finishing generation class

--- marvin/lib/generation.py
+++ marvin/lib/generation.py
@@ -15,12 +15,53 @@
 
 class Generation(object):
     """
-    ..........
+    Overall properties of a generation of species/population.
     """
-    avg_fit = 0.0
-    min_fit =  1000000
-    max_fit = -1000000
-    max_neural_net = None
-    total_reward = 0
-    action = None
-    best_neural_nets = []
+    def __init__(self):
+        self.avg_fit = 0.0
+        self.min_fit =  1000000
+        self.max_fit = -1000000
+        self.max_neural_net = None
+        self.total_reward = 0
+        self.action = None
+        self.best_neural_nets = []
+        return None
+
+    # Get methods
+    def getAvgFit(self):
+        return self.avg_fit
+    def getMinFit(self):
+        return self.min_fit
+    def getMaxFit(self):
+        return self.max_fit
+    def getMaxNeuralNet(self):
+        return self.max_neural_net
+    def getTotalReward(self):
+        return self.total_reward
+    def getAction(self):
+        return self.action
+    def getBestNeuralNets(self):
+        return self.best_neural_nets
+
+    # Set methods
+    def setAvgFit(self, val):
+        self.avg_fit = val
+        return None
+    def setMinFit(self, val):
+        self.min_fit = val
+        return None
+    def setMaxFit(self, val):
+        self.max_fit = val
+        return None
+    def setMaxNeuralNet(self, val):
+        self.max_neural_net = val
+        return None
+    def setTotalReward(self, val):
+        self.total_reward = val
+        return None
+    def setAction(self, val):
+        self.action = val
+        return None
+    def setBestNeuralNets(self, val):
+        self.best_neural_nets = val
+        return None
