3fc54ded878fec591a713a3f66e66061b25f6d69
Gerardo Solis <gsolis@Gerardos-iMac.local>
2017-09-06 23:45:08 -0700

    Commenting out render, everything should work cool:

--- marvin/marvin.py
+++ marvin/marvin.py
@@ -44,6 +44,10 @@ from lib.utilities import map_range, normalize_array, scale_array, debug_object
 # Global variables.
 GAME_NAME = 'Marvin-v0'
 
+
+
+
+# Debugging functions, remove them before turning in the project!!!
 def print_stats():
     print("Here is when the log is printed to the stdout")
 
@@ -52,20 +56,13 @@ def main(flg):
     Main entry point of the program.
     """
 
-    # Just to see if anything gives me an error as of now.
-    print_stats()
-
-################################################################################
-# Objects and class declarations
-################################################################################
-
     # OpenAIGym class and method declarations
     gym_ai = OpenAIGym(GAME_NAME)
-    max_steps = gym_ai.getMaxSteps()
-    max_generations = gym_ai.getMaxGenerations()
+    max_steps = 1000
+    max_generations = 100
+    population_count = 1000
+    mutation_rate = 0.042
     node_count = gym_ai.getNodeCount()
-    population_count = gym_ai.getPopulationCount()
-    mutation_rate = gym_ai.getMutationRate()
 
     # Generation class declaration
     gen = Generation()
@@ -75,33 +72,30 @@ def main(flg):
     # Population class declaration
     pop = Population(population_count, mutation_rate, node_count)
 
-################################################################################
-
+    # Loop for each generation
     for gen in range(max_generations):
         avg_fit = 0.0
         min_fit =  1000000
         max_fit = -1000000
         max_neural_net = None
-
+        # Loop for each species in the generation
         for nn in pop.population:
             total_reward = 0
             observation = gym_ai.getObservation()
-
+            # Loop for every step taken by Marvin
             for step in range(max_steps):
-                gym_ai.getRender()
+                #gym_ai.getRender()
                 gym_ai.setAction(nn.getOutput(observation))
                 observation, reward, done, info = gym_ai.getAction()
                 total_reward += reward
                 if done:
                     break
-
             nn.fitness = total_reward
             min_fit = min(min_fit, nn.fitness)
             avg_fit += nn.fitness
             if nn.fitness > max_fit:
                 max_fit = nn.fitness
                 max_neural_net = copy.deepcopy(nn);
-
         best_neural_nets.append(max_neural_net)
         avg_fit /= pop.getPopulationCount()
 
@@ -109,15 +103,9 @@ def main(flg):
 
         pop.createNewGeneration(max_neural_net)
 
-################################################################################
-# flg.runFlags here maybe
-################################################################################
-    #flags.recordBestBots(bestNeuralNets, env)
-    #flags.replayBestBots(bestNeuralNets, max(1, int(math.ceil(max_generations/10.0))), 0.0625)
-
-
-
-
+    # Records and replayes the best bots
+    #recordBestBots(best_neural_nets, env)
+    #replayBestBots(bestNeuralNets, max(1, int(math.ceil(max_generations / 10.0))), 0.0625)
 
 if __name__ == "__main__":
     """
